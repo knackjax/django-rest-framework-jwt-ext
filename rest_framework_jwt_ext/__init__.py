4c78c2429cbdbe65fcfdeef6d15d10a0b2f060ec
Jackson Kwok <jax@knackworks.com>
2018-09-12 19:06:48 -0400

    added version init info

---
+++ rest_framework_jwt_ext/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'djangorestframework-jwt-ext'
+__version__ = '0.1.0'
+__author__ = 'Jackson Kwok'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 Knack Works Inc'
+
+# Version synonym
+VERSION = __version__
